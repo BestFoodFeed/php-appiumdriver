80e1bb4faf7c4e16eafcfdcddbdc08dc694bb07d
Calvin Schemanski <calvin@myfab5.com>
2021-07-15 17:15:37 -0400

    add set clipboard

--- lib/Remote/RemoteAppiumDriver.php
+++ lib/Remote/RemoteAppiumDriver.php
@@ -3,5 +3,8 @@
 namespace Facebook\WebDriver\Remote;
 
 class RemoteAppiumDriver extends RemoteWebDriver {
-    
+    public function setClipboard($text)
+    {
+        $this->executeCustomCommand("/session/:sessionId/appium/device/set_clipboard",'POST',['content' => base64_encode($text)]);
+    }
 }
