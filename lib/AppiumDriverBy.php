2693b78d0e02cb4d219c1e652dc40b80ba0ef617
Calvin Schemanski <calvin@myfab5.com>
2021-07-15 16:08:40 -0400

    appium classes

---
+++ lib/AppiumDriverBy.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Facebook\WebDriver;
+
+/**
+ * The basic 8 mechanisms supported by webdriver to locate a web element.
+ * ie. 'class name', 'css selector', 'id', 'name', 'link text',
+ *     'partial link text', 'tag name' and 'xpath'.
+ *
+ * @see WebDriver::findElement, WebDriverElement::findElement
+ */
+class AppiumDriverBy
+{
+    /**
+     * @var string
+     */
+    private $mechanism;
+    /**
+     * @var string
+     */
+    private $value;
+
+    protected function __construct($mechanism, $value)
+    {
+        $this->mechanism = $mechanism;
+        $this->value = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMechanism()
+    {
+        return $this->mechanism;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+
+    public static function accessibilityId($id)
+    {
+        return new static('accessibility id', $id);
+    }
+
+    public static function text($text)
+    {
+        return new static('-android uiautomator', 'new UiSelector().text("'.$text.'")');
+    }
+
+
+    /**
+     * Locates elements whose class name contains the search value; compound class
+     * names are not permitted.
+     *
+     * @param string $class_name
+     * @return static
+     */
+    public static function className($class_name)
+    {
+        return new static('class name', $class_name);
+    }
+
+    /**
+     * Locates elements whose ID attribute matches the search value.
+     *
+     * @param string $id
+     * @return static
+     */
+    public static function id($id)
+    {
+        return new static('id', $id);
+    }
+
+    /**
+     * Locates elements whose NAME attribute matches the search value.
+     *
+     * @param string $name
+     * @return static
+     */
+    public static function name($name)
+    {
+        return new static('name', $name);
+    }
+
+
+    /**
+     * Locates elements matching an XPath expression.
+     *
+     * @param string $xpath
+     * @return static
+     */
+    public static function xpath($xpath)
+    {
+        return new static('xpath', $xpath);
+    }
+}
