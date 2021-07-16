65e93d72b03c844d5514c09f24c0309b3e709d86
Calvin Schemanski <calvin@myfab5.com>
2021-07-16 10:55:28 -0400

    add driver functions

--- lib/Remote/RemoteAppiumDriver.php
+++ lib/Remote/RemoteAppiumDriver.php
@@ -2,9 +2,28 @@
 
 namespace Facebook\WebDriver\Remote;
 
-class RemoteAppiumDriver extends RemoteWebDriver {
+class RemoteAppiumDriver extends RemoteWebDriver
+{
     public function setClipboard($text)
     {
-        $this->executeCustomCommand("/session/:sessionId/appium/device/set_clipboard",'POST',['content' => base64_encode($text)]);
+        return $this->executeCustomCommand("/session/:sessionId/appium/device/set_clipboard", 'POST', ['content' => base64_encode($text)]);
+    }
+
+    public function paste()
+    {
+        return $this->executeShell("input keyevent 279");
+    }
+
+    public function executeShell($command)
+    {
+        return $this->executeCustomCommand("/session/:sessionId/execute", 'POST', [
+            "script" => "mobile: shell",
+            "args" => ["command" => $command]
+        ]);
+    }
+
+    public function pushFile(string $path, string $data)
+    {
+        return $this->executeCustomCommand("/session/:sessionId/appium/device/push_file",'POST', ['path' => $path, 'data' => $data]);
     }
 }
